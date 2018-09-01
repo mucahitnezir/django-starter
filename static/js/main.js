05200fca4a0a62d3ef7496f20a0cd4c6e102d40e
Mücahit Nezir <mucahitnezir@gmail.com>
2018-09-01 16:06:00 +0300

    Fixed portfolio isotope bug.

--- static/js/main.js
+++ static/js/main.js
@@ -2,14 +2,15 @@ $(document).ready(function () {
 
     // Portfolio isotope
     if ($.fn.isotope) {
-        var $grid = $('#portfolio-list').isotope({
+        const $grid = $('#portfolio-list').isotope({
             itemSelector: '.portfolio-list-item'
         });
 
         $('#portfolio-filter-buttons').on('click', 'button', function () {
-            var filterValue = $(this).data('filter');
+            const filterValue = $(this).data('filter');
             $grid.isotope({filter: filterValue});
         });
+        $grid.isotope({filter: '*'});
     }
 
 });
