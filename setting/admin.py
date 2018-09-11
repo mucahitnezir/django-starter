210e331f92bc574c2345199dd3b4a5fd2277dcf1
Mücahit Nezir <mucahitnezir@gmail.com>
2018-09-12 01:03:18 +0300

    Setting app admin configuration..

--- setting/admin.py
+++ setting/admin.py
@@ -4,10 +4,14 @@ from .models import Setting
 
 class SettingAdmin(admin.ModelAdmin):
     list_display = ['id', 'key', 'description', 'admin_list_value', 'updated_at']
+    list_filter = ['updated_at']
     search_fields = ['key', 'value']
 
     class Meta:
         model = Setting
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(Setting, SettingAdmin)
