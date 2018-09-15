ec4f14ed0b0b1d203b840bd57814210d35d6cb68
Mücahit Nezir <mucahitnezir@gmail.com>
2018-09-15 21:11:40 +0300

    Database

--- baseapp/setting/production.py
+++ baseapp/setting/production.py
@@ -10,14 +10,21 @@ ALLOWED_HOSTS = ['mucahitnezir.pythonanywhere.com']
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.setdefault('DB_HOST', 'localhost'),
+#         'PORT': os.environ.setdefault('DB_PORT', '5432')
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.setdefault('DB_HOST', 'localhost'),
-        'PORT': os.environ.setdefault('DB_PORT', '5432')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
