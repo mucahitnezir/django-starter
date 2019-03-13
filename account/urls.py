22a7dede9fd39e69a3651fa5e64b1cdd73a166b2
Mücahit Nezir <mucahitnezir@gmail.com>
2019-03-14 01:45:42 +0300

    Improved user register view (class based view)

--- account/urls.py
+++ account/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import views as auth_views
 
-from .views import ProfileEditView, register_view
+from .views import ProfileEditView, RegisterView
 
 app_name = 'account'
 
 urlpatterns = (
     path(_('login/'), auth_views.LoginView.as_view(template_name="account/login.html"), name='login'),
-    path(_('register/'), register_view, name='register'),
+    path(_('register/'), RegisterView.as_view(), name='register'),
     path(_('logout/'), auth_views.LogoutView.as_view(), name='logout'),
     path(_('edit-profile/'), ProfileEditView.as_view(), name='edit-profile'),
 )
--- account/views.py
+++ account/views.py
@@ -1,34 +1,40 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from django.utils.translation import ugettext as _
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, CreateView
 
 from .forms import RegisterForm, EditProfileForm
 
 
-def register_view(request):
-    form = RegisterForm(request.POST or None)
-    if form.is_valid():
+class RegisterView(SuccessMessageMixin, CreateView):
+    model = User
+    form_class = RegisterForm
+    template_name = 'account/register.html'
+    success_url = reverse_lazy('home')
+    success_message = _('Successful signup')
+    extra_context = {'title': _('Signup')}
+
+    def form_valid(self, form):
+        # Create user row
         user = form.save(commit=False)
         password = form.cleaned_data.get('password1')
         user.set_password(password)
-        # user.is_staff = True # Panele giriş yapabilmek için
-        # user.is_superuser = True # Süper yetkili kullanıcı için
+        # user.is_staff = True
+        # user.is_superuser = True
         user.save()
+        # Login
         new_user = authenticate(username=user.username, password=password)
-        login(request, new_user)
-        messages.success(request, _('Successful signup'))
-        return redirect('home')
+        login(self.request, new_user)
+        return super().form_valid(form)
 
-    context = {
-        'form': form,
-        'title': _('Signup')
-    }
-    return render(request, 'account/register.html', context)
+    def form_invalid(self, form):
+        messages.warning(self.request, 'Üyelik kaydı oluşturulamadı')
+        return super().form_invalid(form)
 
 
 class ProfileEditView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
