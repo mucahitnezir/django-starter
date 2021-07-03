2b5b571da19c37941ae1634d0bd26ac8b0063b7d
Mücahit Nezir <mucahitnezir@gmail.com>
2021-07-03 17:53:39 +0300

    Improved blog and category admin pages

--- blog/admin.py
+++ blog/admin.py
@@ -6,29 +6,18 @@ from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'slug', 'category', 'get_user', 'published_at')
+    list_display = ('id', 'title', 'slug', 'category', 'user', 'published_at')
     list_display_links = ('id',)
-    list_editable = ('title', 'category')
     list_filter = ('published_at', 'category')
+    list_select_related = ('category', 'user')
     prepopulated_fields = {'slug': ('title',)}
-    search_fields = ('title', 'content')
-
-    def get_user(self, obj):
-        return obj.user.get_full_name()
-
-    get_user.admin_order_field = 'user'  # Allows column order sorting
-    get_user.short_description = _('Author')  # Renames column head
+    search_fields = ('title', 'category__name')
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'full_name', 'email_address', 'get_post', 'published_at', 'is_confirmed')
+    list_display = ('id', 'full_name', 'email_address', 'post', 'published_at', 'is_confirmed')
     list_editable = ('is_confirmed',)
     list_filter = ('is_confirmed', 'post')
+    list_select_related = ('post',)
     search_fields = ('full_name', 'email_address', 'content')
-
-    def get_post(self, obj):
-        return obj.post.title
-
-    get_post.admin_order_field = 'post'
-    get_post.short_description = _('Post')
--- category/admin.py
+++ category/admin.py
@@ -6,7 +6,6 @@ from .models import Category
 @admin.register(Category)
 class CategoryAdmin(ExportActionModelAdmin):
     list_display = ('id', 'name', 'slug', 'type', 'published_at')
-    list_editable = ('name', 'type')
     list_filter = ('published_at', 'type')
     prepopulated_fields = {'slug': ('name',)}
     search_fields = ('name', 'slug')
